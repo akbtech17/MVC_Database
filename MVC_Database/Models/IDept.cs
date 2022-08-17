c9f7342c29e845a3980b02f22a1d0d4cb59cb76b
anshul bansal <akbt.tech17@gmail.com>
2022-08-17 08:15:17 +0530

    Created IDept Interface

---
+++ MVC_Database/Models/IDept.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace MVC_Database.Models
+{
+    public interface IDept
+    {
+        List<Dept> GetDepts();
+        Dept FindDept(int id);
+        void AddDept(Dept dept);
+        void EditDept(Dept dept);
+        void DeleteDept(int id);
+
+    }
+}
