b2140dc0f064b5b438dbf8900e3b50427e276818
anshul bansal <akbt.tech17@gmail.com>
2022-08-17 14:19:36 +0530

    Used DataAnnotations : DisplayName in Dept Class

--- MVC_Database/Models/Dept.cs
+++ MVC_Database/Models/Dept.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 #nullable disable
 
@@ -12,8 +13,13 @@ namespace MVC_Database.Models
             Emps = new HashSet<Emp>();
         }
 
+        [Display(Name = "Department Code")]
         public int Id { get; set; }
+
+        [Display(Name = "Department Name")]
         public string Name { get; set; }
+
+        [Display(Name = "Department Location")]
         public string Location { get; set; }
 
         public virtual ICollection<Emp> Emps { get; set; }
