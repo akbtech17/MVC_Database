4981164a736e3594878cb5a3f786b45e7d13e6b0
anshul bansal <akbt.tech17@gmail.com>
2022-08-17 10:18:45 +0530

    Injected DeptRepository into DeptController using Constructor

--- MVC_Database/Controllers/DeptController.cs
+++ MVC_Database/Controllers/DeptController.cs
@@ -1,9 +1,14 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MVC_Database.Models;
 
 namespace MVC_Database.Controllers
 {
     public class DeptController : Controller
     {
+        IDept repos;
+        public DeptController(IDept _repos) { 
+               repos = _repos;
+        }
         public IActionResult Index()
         {
             return View();
