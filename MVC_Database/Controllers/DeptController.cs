ce2a1ccf97015e34bce4c229e1b9186cb63a556f
anshul bansal <akbt.tech17@gmail.com>
2022-08-17 10:14:51 +0530

    Added ref to viewimports Created new controller

---
+++ MVC_Database/Controllers/DeptController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC_Database.Controllers
+{
+    public class DeptController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
