1802e67823fefa92a5230547bd99f45e98c6f6dd
anshul bansal <akbt.tech17@gmail.com>
2022-08-17 08:16:28 +0530

    Created a DeptRepo and  implemented IDept Interface in it

---
+++ MVC_Database/Models/DeptRepository.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace MVC_Database.Models
+{
+    public class DeptRepository : IDept
+    {
+        public void AddDept(Dept dept)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void DeleteDept(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void EditDept(Dept dept)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Dept FindDept(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public List<Dept> GetDepts()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
