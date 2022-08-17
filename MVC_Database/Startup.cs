7887d1eda0b225b5957cc9f2d28d1ad61447643e
anshul bansal <akbt.tech17@gmail.com>
2022-08-17 08:05:52 +0530

    Dependency Injection

--- MVC_Database/Startup.cs
+++ MVC_Database/Startup.cs
@@ -1,8 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MVC_Database.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,10 +15,19 @@ namespace MVC_Database
 {
     public class Startup
     {
+        IConfiguration config;
+
+        public Startup(IConfiguration configuration)
+        {
+            config = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<db1045Context>(db => db.UseSqlServer(config.GetConnectionString("mycon")));
+            services.AddMvc(ep => ep.EnableEndpointRouting=false);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,14 +38,15 @@ namespace MVC_Database
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseStaticFiles();
             app.UseRouting();
 
-            app.UseEndpoints(endpoints =>
+            app.UseMvc(routes =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                routes.MapRoute(
+                        name: "default",
+                        template: "{controller-Dept}/{action-List}/{id?}"
+                    );
             });
         }
     }
