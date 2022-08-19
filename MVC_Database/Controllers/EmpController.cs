7388b20b993c5ba5859e3534e9f9c2ccc778612f
anshul bansal <akbt.tech17@gmail.com>
2022-08-19 09:55:15 +0530

    Created EmpController

---
+++ MVC_Database/Controllers/EmpController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_Database.Models;
+
+namespace MVC_Database.Controllers
+{
+    public class EmpController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
