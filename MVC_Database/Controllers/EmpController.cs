dbb2c0d41e03d8844cee154542f778d42625d68d
anshul bansal <akbt.tech17@gmail.com>
2022-08-19 12:57:43 +0530

    Edit View is Created

--- MVC_Database/Controllers/EmpController.cs
+++ MVC_Database/Controllers/EmpController.cs
@@ -33,5 +33,30 @@ namespace MVC_Database.Controllers
             ViewBag.Deptid = new SelectList(db.Depts, "Id", "Name");
             return View(emp);
         }
+
+        [HttpGet]
+        public IActionResult Edit(int id) {
+            var empdata = db.Emps.Find(id);
+            ViewBag.Deptid = new SelectList(db.Depts, "Id", "Name");
+            return View(empdata);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Emp emp)
+        {
+            if (ModelState.IsValid) {
+                var odata = db.Emps.Find(emp.Id);
+                odata.Name = emp.Name;
+                odata.Salary = emp.Salary;
+                odata.Email = emp.Email;
+                odata.Dob = emp.Dob;
+                odata.Deptid = emp.Deptid;
+                db.SaveChanges();
+                return RedirectToAction("List");
+            }
+           
+            ViewBag.Deptid = new SelectList(db.Depts, "Id", "Name");
+            return View(emp);
+        }
     }
 }
