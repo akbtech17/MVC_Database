2476b11dead39100ab61a3ba5a8af8a70f4ff06d
anshul bansal <akbt.tech17@gmail.com>
2022-08-19 09:43:23 +0530

    Working with Emp Model Adding data annotations for validations in Emp Model

--- MVC_Database/Models/Emp.cs
+++ MVC_Database/Models/Emp.cs
@@ -1,17 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using System.ComponentModel.DataAnnotations;
 #nullable disable
 
 namespace MVC_Database.Models
 {
     public partial class Emp
     {
+        [Display(Name = "Emp-Code")]
         public int Id { get; set; }
+
+        [Display(Name = "Emp-Name")]
+        [Required(ErrorMessage = "Cannot be empty!")]
         public string Name { get; set; }
+
+        [Display(Name = "Emp-CTC")]
+        [Range(200000,10000000, ErrorMessage = "Value should be in range 200000-10000000")]
         public int? Salary { get; set; }
+
+        [Display(Name = "Dept-Code")]
         public int? Deptid { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:yyyy-MM-dd}")]
         public DateTime? Dob { get; set; }
+
+        [Required(ErrorMessage = "Cannot be empty!")]
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
 
         public virtual Dept Dept { get; set; }
