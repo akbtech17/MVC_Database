7d72e33fe71632be7ca8a6ae2ae9a7a686000a43
anshul bansal <akbt.tech17@gmail.com>
2022-08-20 10:02:38 +0530

    Displaying the actual Exception Message

--- MVC_Database/Controllers/HomeController.cs
+++ MVC_Database/Controllers/HomeController.cs
@@ -14,6 +14,7 @@ namespace MVC_Database.Controllers
             }
             catch(Exception e) 
             {
+                ViewBag.ErrorMessage = e.Message;
                 return View("Error");
             }
             return View();
